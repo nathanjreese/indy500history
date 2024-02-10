f0a19917476dee010280c65b2cec635ed100e578
Nathan <nathanjreese@gmail.com>
2024-02-09 21:02:41 -0500

    env stuff

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,7 @@ defineProps({
 })
 
 const count = ref(0)
+const test_env = import.meta.env.VITE_TEST_VALUE
 </script>
 
 <template>
@@ -15,7 +16,7 @@ const count = ref(0)
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code>components/HelloWorld.vue</code> to test {{ test_env }}
     </p>
   </div>
 
